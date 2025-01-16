6b71a10a6f51d67bb015cfdfa19f0b30bf371061
josealbertobritosantana <josealbertobritosantana@gmail.com>
2025-01-16 23:03:21 +0000

    error handler added

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -6,19 +6,35 @@ const list = document.querySelector("ul");
 
 //adding process
 button.addEventListener("click", function() {
-    const deleItem = document.createElement("button");
-    deleItem.textContent = "❌";
-    deleItem.ariaLabel = "Remove chapter";    
-    deleItem.setAttribute("class", "delete");
-//removing process
-    deleItem.addEventListener("click", function() {
-        list.removeChild(this.parentNode);
-    });
-//appending process
-    const nItem = document.createElement("li");
-    nItem.textContent = input.value;
-    nItem.appendChild(deleItem);
-    list.appendChild(nItem);
+    const empty = document.getElementById("errorlog")
+    if (input.value === "") {
+    //--------------------------addings
+        empty.textContent = "Please enter a chapter";
+        empty.style.color = "red";
+        empty.style.fontSize = "0.7rem";
+        empty.style.textAlign = "center";
+
+        // setTimeout(function() {
+        //     empty.textContent = "";
+        //     empty.style.color = "black";
+        // }, 2000);
+    } else{
+        empty.textContent = "";
+        const deleItem = document.createElement("button");
+        deleItem.textContent = "❌";
+        deleItem.ariaLabel = "Remove chapter";    
+        deleItem.setAttribute("class", "delete");
+        //removing process
+        deleItem.addEventListener("click", function() {
+            list.removeChild(this.parentNode);
+        });
+        //appending process
+        const nItem = document.createElement("li");
+        nItem.textContent = input.value;
+        nItem.appendChild(deleItem);
+        list.appendChild(nItem);
+    }
     input.value = "";
+    input.focus();
 });
 
