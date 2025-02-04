d566373faae6e67ab329d746d7e8489874ae1bc1
Jose1103-lab <josealbertobritosantana@gmail.com>
2025-02-04 08:42:45 -0400

    layout done

--- scripts/filtered.js
+++ scripts/filtered.js
@@ -1,12 +1,36 @@
 // DATASET FOR THE TEMPLES
 const temples = [
   {
+    templeName: "Kirtland Ohio",
+    location: "Kirtland, Ohio, United States",
+    dedicated: "1836, March, 27",
+    area: 15000,
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/kirtland-temple/kirtland-temple-1275-main.jpg"
+  },
+  {
+    templeName: "Frankfurt Germany",
+    location: "Frankfurt, Germany",
+    dedicated: "2019, October, 20",
+    area: 32895,
+    imageUrl:
+      "https://churchofjesuschristtemples.org/assets/img/temples/frankfurt-germany-temple/frankfurt-germany-temple-6612.jpg"
+  },
+  {
+    templeName: "Calgary Alberta",
+    location: "Calgary, Alberta, Canada",
+    dedicated: "2012, October, 28",
+    area: 33000,
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+  },
+  {
     templeName: "Aba Nigeria",
     location: "Aba, Nigeria",
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
   },
   {
     templeName: "Manti Utah",
@@ -14,7 +38,7 @@ const temples = [
     dedicated: "1888, May, 21",
     area: 74792,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
   },
   {
     templeName: "Payson Utah",
@@ -22,7 +46,7 @@ const temples = [
     dedicated: "2015, June, 7",
     area: 96630,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
   },
   {
     templeName: "Yigo Guam",
@@ -30,7 +54,7 @@ const temples = [
     dedicated: "2020, May, 2",
     area: 6861,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
   },
   {
     templeName: "Washington D.C.",
@@ -38,7 +62,7 @@ const temples = [
     dedicated: "1974, November, 19",
     area: 156558,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
   },
   {
     templeName: "Lima Perú",
@@ -46,7 +70,7 @@ const temples = [
     dedicated: "1986, January, 10",
     area: 9600,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
   },
   {
     templeName: "Mexico City Mexico",
@@ -54,29 +78,54 @@ const temples = [
     dedicated: "1983, December, 2",
     area: 116642,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
   // Add more temple objects here...
 ];
+
+
+
+
 temples.forEach(item => {
-    const container = document.querySelector(".img-container");
-    const figure = document.createElement("figure")
-    const caption = document.createElement("figcaption")
-    const image = document.createElement("img")
-    
-    caption.textContent = item.templeName;
-    
-    figure.appendChild(image);
-    figure.appendChild(caption);
-    container.appendChild(figure);
-    console.log(item.area)
+  const container = document.querySelector(".img-container");
+  const figure = document.createElement("figure");
+  const caption = document.createElement("figcaption");
+  const image = document.createElement("img");
+  const list = document.createElement("ul");
+  const lItemDate = document.createElement("li");
+  const lItemArea = document.createElement("li");
+  const lItemLocation = document.createElement("li");
+
+  lItemDate.innerHTML = (`<span class="bolden">Dedicated:</span> ${item.dedicated}`);
+  lItemArea.innerHTML = (`<span class="bolden">Area:</span> ${item.area}`)
+  lItemLocation.innerHTML = (`<span class="bolden">Location:</span> ${item.location}`);
+
+  list.appendChild(lItemDate);
+  list.appendChild(lItemArea);
+  list.appendChild(lItemLocation);
+
+  image.setAttribute("src", `${item.imageUrl}`);
+  image.setAttribute("alt", `${item.templeName}`);
+  image.setAttribute("loading", "lazy");
+  image.width = "250";
+  image.height = "200";
+
+  caption.textContent = item.templeName;
+  caption.classList.add("rancho-regular");
+  list.classList.add("gabriela-regular");
+
+  figure.appendChild(image);
+  figure.appendChild(caption);
+  figure.appendChild(list);
+  container.appendChild(figure);
+  console.log(item.area);
 });
 
 
 const navigator = document.querySelector('.navigator');
 const displayer = document.querySelector('#menu');
 
-displayer.addEventListener('click', () => { 
-    displayer.classList.toggle('show');
-    navigator.classList.toggle('show');
+displayer.addEventListener('click', () => {
+  displayer.classList.toggle('show');
+  navigator.classList.toggle('show');
 });
