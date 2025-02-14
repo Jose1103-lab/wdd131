182c02f45df5300d110f3943e7e46091af2e2e1d
Jose1103-lab <josealbertobritosantana@gmail.com>
2025-02-14 08:18:44 -0400

    visit counter missing

--- scripts/form.js
+++ scripts/form.js
@@ -1,32 +1,58 @@
-let products = [
-    "iPhone 13",
-    "Samsung Galaxy S21",
-    "MacBook Pro",
-    "Dell XPS 13",
-    "Apple Watch Series 7",
-    "Sony WH-1000XM4",
-    "iPad Pro",
-    "Amazon Echo",
-    "Google Nest Hub",
-    "Microsoft Surface Pro 7",
-    "PlayStation 5",
-    "Xbox Series X",
-    "Nvidia RTX 3080",
-    "Kindle Paperwhite",
-    "Raspberry Pi 4"
-]; 
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
 
 // working with the dom =
+
 function optionGenertor(array){
     const optionSet = document.querySelector('#products');
     const newLocal = array.sort(); // extracting a sorted array to a new
     newLocal.forEach(element => {
         const nItem = document.createElement("option");
-        nItem.textContent = element;
-        nItem.setAttribute("value", element.toLowerCase())    
+        nItem.textContent = element.name;
+        nItem.setAttribute("value", element.name.toLowerCase())  
+        nItem.setAttribute("id", element.name.toLowerCase().replace(/\s+/g, '-'));
         optionSet.appendChild(nItem);
-        //! console.log(element);
+        //! console.log(element.name);
     });
 }
 
+const autReview = document.querySelector(".complete");
+
+const tester = autReview.textContent;
+
+console.log(autReview.innerHTML);
+
+if(tester === "Review Completed"){
+    let visits = Number(window.localStorage.getItem('visitAs')) || 0;
+    visits += 1;
+    localStorage.setItem('visitAs', visits);
+    console.log("done");
+}
 optionGenertor(products);
+
+
