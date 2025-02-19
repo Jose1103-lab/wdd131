218b8afdc02a3d15b91b5cdf72d1d305e0400544
Jose1103-lab <josealbertobritosantana@gmail.com>
2025-02-19 09:10:06 -0400

    script 2/3

--- project/scripts/base.js
+++ project/scripts/base.js
@@ -1,4 +1,81 @@
+// an array of object that includes the designs description
+const products = [
+    { 
+        name: "Item 1", 
+        description: "Description 1", 
+        category: "Category 1", 
+        url: "http://example.com/1" 
+    },
+    { 
+        name: "Item 2", 
+        description: "Description 2", 
+        category: "Category 2", 
+        url: "http://example.com/2" 
+    },
+    { 
+        name: "Item 3", 
+        description: "Description 3", 
+        category: "Category 3", 
+        url: "http://example.com/3" 
+    },
+    { 
+        name: "Item 4", 
+        description: "Description 4", 
+        category: "Category 4", 
+        url: "http://example.com/4" 
+    },
+    { 
+        name: "Item 5", 
+        description: "Description 5", 
+        category: "Category 5", 
+        url: "http://example.com/5" 
+    },
+    { 
+        name: "Item 6", 
+        description: "Description 6", 
+        category: "Category 6", 
+        url: "http://example.com/6"  
+    },
+    { 
+        name: "Item 7", 
+        description: "Description 7", 
+        category: "Category 7", 
+        url: "http://example.com/7" 
+    },
+    { 
+        name: "Item 8", 
+        description: "Description 8", 
+        category: "Category 8", 
+        url: "http://example.com/8" 
+    },
+    { 
+        name: "Item 9", 
+        description: "Description 9", 
+        category: "Category 9", 
+        url: "http://example.com/9" 
+    },
+    { 
+        name: "Item 10",
+        description: "Description 10", 
+        category: "Category 10", 
+        url: "http://example.com/10" 
+    }
+];
+
+
+// this function retrive three random designs to our home page.
+
+
+// this function retrieves of the product with the manipulation of the DOM
+
+
+// function that works with the responsive menu
 function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("show");
 }
+
+// function that counts the amount of visit to our webpage and reads and stores them in the user locaa storage.
+
+
+// function that gets the date.
