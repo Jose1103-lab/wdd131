78214075df794f50fe9e23f45106a48ba0afe2dd
josealbertobritosantana <josealbertobritosantana@gmail.com>
2025-02-26 22:53:14 +0000

    self web update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,11 @@
-let lastMod = new Date(document.lastModified);
+// let lastMod = new Date(document.lastModified);
+// let modilabel = document.getElementById("lastMod");
 let currentYear = new Date();
-let modilabel = document.getElementById("lastMod");
 let copyrightY = document.getElementById("year");
 
 
 try {
-    modilabel.innerHTML = `Last modification: ${lastMod}`;
+    // modilabel.innerHTML = `Last modification: ${lastMod}`;
     copyrightY.innerHTML = currentYear.getFullYear()
 } catch (error) {
     console.log('>_ date setter failed, make sure this page has a footer');
