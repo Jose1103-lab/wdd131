ef3406623049a35e8edbee00e2d9578e2ebe81ed
josealbertobritosantana <josealbertobritosantana@gmail.com>
2025-03-03 13:43:39 +0000

    repo change

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,14 @@
-// let lastMod = new Date(document.lastModified);
-// let modilabel = document.getElementById("lastMod");
+/* ----- in case i want to know the last time was updated--------- 
+let lastMod = new Date(document.lastModified);
+ let modilabel = document.getElementById("lastMod");
+ modilabel.innerHTML = `Last modification: ${lastMod}`;
+*/
+
+
 let currentYear = new Date();
 let copyrightY = document.getElementById("year");
 
-
 try {
-    // modilabel.innerHTML = `Last modification: ${lastMod}`;
     copyrightY.innerHTML = currentYear.getFullYear()
 } catch (error) {
     console.log('>_ date setter failed, make sure this page has a footer');
